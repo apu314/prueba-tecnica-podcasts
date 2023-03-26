2c9a4becc5f6b982f708cc5c1539d60b56bfeaed
Adolfo Unturbe <apu314@gmail.com>
2023-03-26 04:45:11 +0100

    tailwind config

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+
+module.exports = {
+  plugins: {
+    tailwindcss: { config: './tailwind.config.cjs' },
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+  }
+}
