9922c234f124ed1859b4de8e643e1f4114c22bd9
Adolfo Unturbe <apu314@gmail.com>
2023-03-27 23:51:15 +0100

    add constants and date-and-time package

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+export const DAY_IN_MILISECONDS = 1000 * 60 * 60 * 24
+
+export const LOCALSTORAGE_LAST_REQUESTED_DATE = 'last-resquested-date'
+export const LOCALSTORAGE_PODCASTS = 'podcasts'
+export const LOCALSTORAGE_PODCASTS_EPISODES = 'podcasts-episodes'
+
+export const PODCASTS_URL = `https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json`
